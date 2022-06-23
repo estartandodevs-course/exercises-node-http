e76c6d09ede69d1ad657dda2a77a43901ba4fa7a
maluvasc <mluisadav@gmail.com>
2022-06-23 16:55:49 -0300

    fixes

--- src/server.js
+++ src/server.js
@@ -44,18 +44,8 @@ switch(req.url){
 }
 }
 
-const server = http.createServer(showWebpage) 
+const server = http.createServer(showWebpage);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-/*
-
-  - TODO 1: Criar um servidor que recebe requisições HTTP na porta 8000 e responde com o conteúdo de um arquivo HTML.
-  - TODO 2: Se for acessada a URL http://localhost:8000/about deve mostrar o conteúdo da pagina `pages/about.html`
-  - TODO 3: Se for acessada a URL http://localhost:8000/ ou http://localhost:8000/home deve mostrar o conteúdo da pagina `pages/index.html`
-  - TODO 4: Se for acessada qualquer outro caminho deve mostrar o conteúdo da pagina `pages/404.html`
- 
-
-  OBS: Deve ser utilizado apenas os módulos nativos do NODE (http, path, fs, etc), nada de instalar outras libs ( ˘︹˘ )
-*/
